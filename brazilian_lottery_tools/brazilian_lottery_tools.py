5c86da9daded2edb14ca17ac3ea76e49c829445a
Mauricio Vieira <mauricio@mauriciovieira.net>
2019-03-26 21:41:29 -0300

    Config

--- brazilian_lottery_tools/brazilian_lottery_tools.py
+++ brazilian_lottery_tools/brazilian_lottery_tools.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from configparser import ConfigParser
+from importlib import resources  # Python 3.7+
+
+
+def main():
+    cfg = ConfigParser()
+    cfg.read_string(resources.read_text("brazilian_lottery_tools", "config.txt"))
